04f1aabe182a287a83ebda6ef844d9c5412171a6
sujan135 <banjara.sujan12@outlook.com>
2023-06-22 13:35:28 -0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 import { useState } from "react";
 import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
 export default function App () {
-  const [newItem, setNewItem] = useState ("")
-  const [todos, setTodos] = useState ([])
+const [todos, setTodos] = useState ([])
 
-  
-  function handleSubmit (e) {
-    e.preventDefault()
-
-    setTodos((currenttodos) => {
-      return[
-        ...currenttodos, 
-        {id:crypto.randomUUID(),
-        title: newItem,
-       completed: false},
-    ]
-  })
-
-  setNewItem('')
+function addTodo (title) {
+    setTodos(currenttodos => {
+      return [
+        ...currenttodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
 }
+  
+  
 
 function toggleTodo (id, completed) {
   setTodos (currenttodos => {
@@ -40,28 +36,9 @@ function deleteTodo (id) {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"></input>
-        </div>
-        <button className="btn">Add</button>
-      </form>
+     <NewTodoForm onSubmit={addTodo}/>
       <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {todos.length ===0 && "No Todos"}
-        {todos.map(todo => {
-          return (
-          <li key={todo.id}>
-          <label>
-            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo (todo.id, e.target.checked)}></input>
-            {todo.title}
-          </label>
-          <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-        </li>
-          )
-        })}
-        </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
     </>
   );
 }
